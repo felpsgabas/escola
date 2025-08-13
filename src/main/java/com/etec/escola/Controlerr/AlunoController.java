2f417961a9eebbd52b0b55f1282439e43b18314a
Felipe Gabriel <felipe27gabrieloliveira13@gmail.com>
2025-08-13 17:25:07 -0300

    feat: classe controlle2

--- src/main/java/com/etec/escola/Controlerr/AlunoController.java
+++ src/main/java/com/etec/escola/Controlerr/AlunoController.java
@@ -1,4 +1,11 @@
 package com.etec.escola.Controlerr;
 
-public class AlunoControler {
+import com.etec.escola.Service.AlunoService;
+
+public class AlunoController {
+    private final AlunoService alunoService;
+
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
 }
